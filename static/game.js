82b9fc4dae5f8f6adda665897279ab320916eab3
Freya <freya@stubkjaer.co.uk>
2020-06-30 16:53:24 -0700

    Quick deploy to Heroku

--- static/game.js
+++ static/game.js
@@ -1,31 +1,59 @@
-let player = {}
+let player = {};
+let items = [];
+let timeRemaining = 45;
+let interval = setInterval(decreaseTimer, 1000);
 
+function decreaseTimer() {
+    timeRemaining--;
+    if (timeRemaining == 0) {
+        clearInterval(interval)
+    }
 
+}
 
 function create(game) {
     player = {
         x: 5,
         y: 10,
     };
-    game.setDot(player.x, player.y, Color.Black)
+    game.setDot(player.x, player.y, Color.Black);
 }
 
 function update(game) {
-    game.setDot(player.x, player.y, Color.Black)
+
+    if (Math.random() < .05) {
+        item = {
+            x: Math.floor(Math.random() * 24),
+            y: Math.floor(Math.random() * 24),
+        };
+        if (item.x !== player.x || item.y !== player.y) {
+            items.push(item);
+        }
+    }
+    for (item of items) {
+        game.setDot(item.x, item.y, Color.Green);
+    }
+    game.setDot(player.x, player.y, Color.Black);
+    game.setText('Time left: ${timeRemaining}s. Score: ${score}s');
+
+    if (timeRemaining <= 0) {
+        game.setText('Game over! Finale score: ${score}');
+        game.end();
+    }
 }
 
 function onKeyPress(direction) {
     if (direction == Direction.Up && player.y > 0) {
-        player.y--
+        player.y--;
     }
     if (direction == Direction.Down && player.y < 23) {
-        player.y++
+        player.y++;
     }
     if (direction == Direction.Left && player.x > 0) {
-        player.x--
+        player.x--;
     }
     if (direction == Direction.Right && player.x < 23) {
-        player.x++
+        player.x++;
     }
 
 
