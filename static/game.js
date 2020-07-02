d01849bb4047e0dbf268b6d95b7eded02a17cf28
Freya <freya@stubkjaer.co.uk>
2020-07-02 16:41:26 -0700

    Adds a toggle mode

--- static/game.js
+++ static/game.js
@@ -2,7 +2,7 @@ let items = [];
 let timeRemaining = 45;
 let interval = setInterval(decreaseTimer, 1000);
 let score = 0;
-
+let active = true
 
 function decreaseTimer() {
     timeRemaining--;
@@ -12,8 +12,8 @@ function decreaseTimer() {
 
 }
 
-
-function create(game) {}
+function create(game) {
+    game.setText('Edit Mode')}
 
 function update(game) {
     let item = {};
@@ -66,39 +66,47 @@ function n_count(x, y) {
 }
 
 function onKeyPress(direction) {
-    newBoard()
+    active ? (active = false) : (active = true)
+    console.log("Switched active to", (active))
+    game.setText(active ? "Edit Mode" : "Sim Mode")
     return;
 
 }
 
 function newBoard() {
-    for (let x = 0;x < 24;x++) {
-        for (let y = 0;y < 24;y++) {
-            let count = n_count(x,y)
-            let dotColor = game.getDot(x,y)
+    for (let x = 0; x < 24; x++) {
+        for (let y = 0; y < 24; y++) {
+            let count = n_count(x, y)
+            let dotColor = game.getDot(x, y)
             if (dotColor == 'BLACK' && count < 2) {
-                let item = {x: x, y: y}
+                let item = {
+                    x: x,
+                    y: y
+                }
                 items.push(item)
-        }
+            }
 
+        }
     }
-    }}
+}
 
 function onDotClicked(x, y) {
-    if (game.getDot(x, y) == 'GRAY') {
-        let item = {
-            x: x,
-            y: y
-        };
-        items.push(item);
-        console.log(n_count(x, y))
+    if (active) {
+        if (game.getDot(x, y) == 'GRAY') {
+            let item = {
+                x: x,
+                y: y
+            };
+            items.push(item);
+            console.log(n_count(x, y))
 
-    } else {
-        for (let i = 0; i < items.length; i++) {
-            const item = items[i]
-            if (item.x == x && item.y == y) {
-                items.splice(i, 1);
-                break;
+        } else {
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i]
+                if (item.x == x && item.y == y) {
+                    items.splice(i, 1);
+                    break;
+                }
             }
         }
     }
